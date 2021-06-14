bf8ebf6f31fe69ae0ec655765ab54349c87fbf6b
Amm0_07 <amoghsawant2000@gmail.com>
2021-06-14 08:31:43 +0530

    final

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -7,41 +7,43 @@ using Unity.MLAgents.Sensors;
 
 public class AIScript : Agent
 {
-    // [SerializeField] private Transform pickUpTransform;
+    public GameObject player;
     private Rigidbody r1;
-    public float totalReward;
     public List<GameObject> allPickups;
-    
+    public bool hitpickup;
+    public bool hitwall;
+    private Vector3 offset;
+
     public override void Initialize()
     {
-        r1 = GetComponent<Rigidbody>();
+        r1 = player.GetComponent<Rigidbody>();
         allPickups = new List<GameObject>(21);
-        
+        offset = transform.position - player.transform.position;
     }
+
     public override void OnEpisodeBegin()
     {
         transform.localPosition = new Vector3(3, 2, -2);
+        player.transform.localPosition = new Vector3(3, 2, -2);
+        
         SetReward(0f);
         r1.velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
         if (allPickups.Count != 0) 
         {
-            
             foreach (GameObject pickUp in allPickups)
             {
+                // pickUp.transform.localPosition = new Vector3(Random.Range(-15.0f, 17.0f), 2, Random.Range(-15.0f, 19.0f));
                 pickUp.SetActive(true);
             }
-
         }
-        allPickups.Clear();
 
+        allPickups.Clear();
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
-        // sensor.AddObservation(pickUpTransform.localPosition);
-
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -55,39 +57,47 @@ public class AIScript : Agent
         
         move = new Vector3(moveX, 0.0f, moveZ);
 
-        r1.AddForce(move * 10);
-
+        r1.AddForce(move * 5);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+
         continousActions[0] = Input.GetAxisRaw("Horizontal");
         continousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
-    private void OnTriggerEnter(Collider other)
+    void FixedUpdate()
     {
-        if (other.gameObject.CompareTag("PickUp") && other.gameObject.activeSelf) 
+        if (hitpickup) 
         {
             AddReward(1f);
-            totalReward = GetCumulativeReward();
-            allPickups.Add(other.gameObject);
-            
-            other.gameObject.SetActive(false);
+            hitpickup = false;          
         }
-        if (other.gameObject.CompareTag("Wall"))
-        {
-            SetReward(-2f);
-            EndEpisode();
 
+        if (hitwall)
+        {
+            AddReward(-2f);
+            hitwall = false;
+            EndEpisode();
         }
-        if (totalReward >= 20f)
+
+        if (GetCumulativeReward() >= 20f)
         {
             AddReward(10f);
             Debug.Log("yay");
             EndEpisode();
         }
 
+        if (r1.velocity.magnitude > 5)
+        {
+            r1.velocity = r1.velocity.normalized * 5;
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
     }
 }
